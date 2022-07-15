eb36c9f01be894b36e23e0bd85c307940ecc899c
famuyiwadayo <famuyiwadayodaniel@gmail.com>
2022-07-15 11:13:21 +0100

    initiated and listen for tcp connection from a tcp client

--- tcp_net/src/main.zig
+++ tcp_net/src/main.zig
@@ -1,19 +1,30 @@
 const std = @import("std");
 const net = std.net;
+const StreamServer = net.StreamServer;
+const Address = net.Address;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
+
+pub const io_mode = .evented;
 
 pub fn main() anyerror!void {
-    // std.log.info("All your codebase are belong to us.", .{});
-    var options: net.StreamServer.Options = {
-        kernel_backlog = 128;
-        reuse_address = true;
-    };
-    const stream_server = net.StreamServer.init(options);
-    defer stream_server.deinit();
+    var gpa = GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    const addr = try net.Address.parseIp("127.0.0.1", 8080);
-    stream_server.listen(addr);
+    var stream_server = StreamServer.init(.{});
+    defer stream_server.close();
+    const addr = try net.Address.resolveIp("127.0.0.1", 8080);
+    try stream_server.listen(addr);
+
+    // const allocator =
+
+    while (true) {
+        const connection = try stream_server.accept();
+        try streamHandler(allocator, connection.stream, connection.address);
+    }
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+fn streamHandler(allocator: std.mem.Allocator, stream: net.Stream, addr: Address) !void {
+    defer stream.close();
+    var Writer = stream.writer();
+    try Writer.print("TCP connected on {}", .{addr});
 }
